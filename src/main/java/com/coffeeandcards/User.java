d9202e532ca50160d06bbdcf0ba594f1923f70cb
Lok Tamang <lb.tamang77@gmail.com>
2022-11-10 14:47:16 -0600

    added user class template

---
+++ src/main/java/com/coffeeandcards/User.java
@@ -0,0 +1,18 @@
+package com.coffeeandcards;
+
+public class User implements IPlayer{
+
+    public int getCard() {
+        return 0;
+    }
+
+    @Override
+    public void keepHand() {
+
+    }
+
+    @Override
+    public int displayValue() {
+        return 0;
+    }
+}
